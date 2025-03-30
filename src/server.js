4c36f7b4eea36ce57a905ef54a2f3592a08531c4
Arina Riabukha <arrin.uq@gmail.com>
2025-03-30 16:45:22 +0300

    services

---
+++ src/server.js
@@ -0,0 +1,57 @@
+import express from "express";
+import cors from "cors";
+import pino from "pino-http";
+import { getContacts, getContactsById } from "./services/contacts.js";
+
+export const setupServer = ()=> {
+    const app = express();
+
+    app.use(cors());
+    app.use(express.json());
+    app.use(pino({
+         transport: {
+             target: "pino-pretty"
+         }
+     }));
+
+     app.get("/contacts", async (req, res)=> {
+        const data = await getContacts();
+
+        res.json({
+            status: 200,
+            message: "Successfully found contacts!",
+            data,
+        });
+    });
+
+    app.get("/contacts/:contactId", async(req, res)=> {
+        const {contactId} = req.params;
+
+        const data = await getContactsById(contactId);
+
+        if(!data) {
+            return res.status(404).json({
+                status: 404,
+                message: `Contact not found`
+            });
+        }
+        res.json({
+            status: 200,
+            message: `Successfully found contact with id ${contactId}!`,
+            data,
+        });
+    });
+
+
+     app.use((req, res)=> {
+        res.status(404).json({
+            message: 'Not found'
+        });
+    });
+
+    const PORT = process.env.PORT || 3000;
+    app.listen(PORT, () => {
+        console.log(`Server is running on port ${PORT}`);
+    });
+
+    };
