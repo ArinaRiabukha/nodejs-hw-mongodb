b276f04c561e9549ff6c507deae56141590d162a
Arina Riabukha <arrin.uq@gmail.com>
2025-04-22 16:14:51 +0300

    fixed patch

--- src/services/contacts.js
+++ src/services/contacts.js
@@ -43,7 +43,7 @@ export const getContactsById = (id, userId) =>
   ContactCollection.findOne({ _id: id, userId });
 export const addContact = (payload) => ContactCollection.create(payload);
 export const updateContact = async (_id, payload) => {
-  return ContactCollection.findByIdAndUpdate(_id, payload);
+  return ContactCollection.findByIdAndUpdate(_id, payload, { new: true });
 };
 export const deleteContactById = (_id) =>
   ContactCollection.findByIdAndDelete(_id);
